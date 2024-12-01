45b2782a7ce0ddd8049c362bf1171cafc4aa21a8
Princy Wamal <princywamal@gmail.com>
2024-12-01 14:30:53 +0100

    Setup and opening the data

--- day1.rb
+++ day1.rb
@@ -1,5 +1,11 @@
 require "open-uri"
 
 url = "https://adventofcode.com/2024/day/1/input"
-lists = URI.open(url)
-p lists
+cookie = "session=53616c7465645f5fd698bebda299f4b28fd4a719fcbd421e327c50a819e733878d9618bb5cc1e5ea6408ae5e569fbadef95ed4461c46292c75ba07c8263ee2f3"
+
+begin
+  lists = URI.open(url, "Cookie" => cookie).read
+  puts lists
+rescue OpenURI::HTTPError => e
+  puts "Failed to fetch input: #{e.message}"
+end
