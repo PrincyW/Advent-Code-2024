964bbbf9c7e46ac91930503ac52c8e80731af9ac
Princy Wamal <princywamal@gmail.com>
2024-12-02 20:21:59 +0100

    Making arrays

--- day2.rb
+++ day2.rb
@@ -7,8 +7,13 @@ url = "https://adventofcode.com/2024/day/2/input"
 cookie = "session=#{ENV['AOC_SESSION']}"
 
 begin
-  records_str = URI.open(url, "Cookie" => cookie).read
-  puts records_str
+  reports_str = URI.open(url, "Cookie" => cookie).read
 rescue OpenURI::HTTPError => e
   puts "Failed to fetch input: #{e.message}"
 end
+
+
+#2 Making arrays
+reports_arr = reports_str.split("\n") # Transforming the string into an array.
+reports = reports_arr.map { |array| array.split.map(&:to_i) }
+puts reports.count == reports_arr.count
